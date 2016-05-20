gorkem/che

Separation of components to allow overrides in codenvy

core/platform-api-client-gwt/che-core-client-gwt-machine/src/main/java/org/eclipse/che/api/machine/gwt/client/DevMachine.java
@@ -11,10 +11,8 @@
 package org.eclipse.che.api.machine.gwt.client;
 
 import com.google.common.base.Strings;
-import com.google.gwt.user.client.Window;
 
-import org.eclipse.che.api.core.model.machine.MachineStatus;
-import org.eclipse.che.api.core.model.machine.Server;
+import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.machine.shared.Constants;
 import org.eclipse.che.api.machine.shared.dto.MachineDto;
 import org.eclipse.che.api.machine.shared.dto.ServerDto;
@@ -22,6 +20,7 @@ import org.eclipse.che.ide.util.loging.Log;
 
 import javax.validation.constraints.NotNull;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,16 +32,15 @@ import java.util.Map;
  */
 public class DevMachine {
 
-    private final MachineDto devMachineDescriptor;
-
+    private final MachineDto                    devMachineDescriptor;
     private final Map<String, DevMachineServer> servers;
-
-    private final Map<String, String> runtimeProperties;
-
-    private final Map<String, String> envVariables;
+    private final Map<String, String>           runtimeProperties;
+    private final Map<String, String>           envVariables;
+    private final List<Link>                    devMachineLinks;
 
     public DevMachine(@NotNull MachineDto devMachineDescriptor) {
         this.devMachineDescriptor = devMachineDescriptor;
+        this.devMachineLinks = devMachineDescriptor.getLinks();
 
         Map<String, ServerDto> serverDtoMap = devMachineDescriptor.getRuntime().getServers();
         servers = new HashMap<>(serverDtoMap.size());
@@ -51,7 +49,6 @@ public class DevMachine {
         }
         runtimeProperties = devMachineDescriptor.getRuntime().getProperties();
         envVariables = devMachineDescriptor.getRuntime().getEnvVariables();
-
     }
 
     public Map<String, String> getEnvVariables() {
@@ -68,18 +65,27 @@ public class DevMachine {
     }
 
     public String getWsAgentWebSocketUrl() {
-        DevMachineServer server = getServer(Constants.WSAGENT_REFERENCE);
-        if (server != null) {
-            String url = server.getUrl();
-            String extUrl = url.substring(url.indexOf(':'), url.length());
-            boolean isSecureConnection = Window.Location.getProtocol().equals("https:");
-            return (isSecureConnection ? "wss" : "ws") + extUrl + "/ws/" + getWorkspace();
-        } else {
-            //should not be
-            String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
-            Log.error(getClass(), message);
-            throw new RuntimeException(message);
+        for (Link devMachineLink : devMachineLinks) {
+            if (Constants.WSAGENT_WEBSOCKET_REFERENCE.equals(devMachineLink.getRel())) {
+                return devMachineLink.getHref();
+            }
         }
+        //should not be
+        final String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
+        Log.error(getClass(), message);
+        throw new RuntimeException(message);
+    }
+
+    public String getTerminalUrl() {
+        for (Link link : devMachineLinks) {
+            if (Constants.TERMINAL_REFERENCE.equals(link.getRel())) {
+                return link.getHref();
+            }
+        }
+        //should not be
+        final String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
+        Log.error(getClass(), message);
+        throw new RuntimeException(message);
     }
 
     public String getWsAgentBaseUrl() {
@@ -98,9 +104,6 @@ public class DevMachine {
         return servers;
     }
 
-
-
-
     public DevMachineServer getServer(String reference) {
         if (!Strings.isNullOrEmpty(reference)) {
             for (DevMachineServer server : servers.values()) {
@@ -112,7 +115,7 @@ public class DevMachine {
         return null;
     }
 
-    public String getWorkspace(){
+    public String getWorkspace() {
         return devMachineDescriptor.getWorkspaceId();
     }
 

wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/WorkspaceServiceTest.java
@@ -51,6 +51,7 @@ import org.everrest.core.RequestFilter;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -108,11 +109,12 @@ import static org.testng.Assert.assertTrue;
 public class WorkspaceServiceTest {
 
     @SuppressWarnings("unused")
-    private static final ApiExceptionMapper MAPPER  = new ApiExceptionMapper();
-    private static final String             USER_ID = "user123";
-    private static final LinkedList<String> ROLES   = new LinkedList<>(singleton("user"));
+    private static final ApiExceptionMapper MAPPER      = new ApiExceptionMapper();
+    private static final String             USER_ID     = "user123";
+    private static final String             IDE_CONTEXT = "ws";
+    private static final LinkedList<String> ROLES       = new LinkedList<>(singleton("user"));
     @SuppressWarnings("unused")
-    private static final EnvironmentFilter  FILTER  = new EnvironmentFilter();
+    private static final EnvironmentFilter  FILTER      = new EnvironmentFilter();
 
     @Mock
     private WorkspaceManager   wsManager;
@@ -122,6 +124,11 @@ public class WorkspaceServiceTest {
     private WorkspaceValidator validator;
     @InjectMocks
     private WorkspaceService   service;
+
+    @BeforeMethod
+    public void setup() {
+        service = new WorkspaceService(wsManager, machineManager, validator, new LinksInjector(IDE_CONTEXT));
+    }
 
     @Test
     public void shouldCreateWorkspace() throws Exception {

ShoOgino/cheFile

core/ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/machine/DevMachine.java
@@ -11,8 +11,8 @@
 package org.eclipse.che.ide.api.machine;
 
 import com.google.common.base.Strings;
-import com.google.gwt.user.client.Window;
 
+import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.machine.shared.Constants;
 import org.eclipse.che.api.machine.shared.dto.MachineDto;
 import org.eclipse.che.api.machine.shared.dto.ServerDto;
@@ -20,6 +20,7 @@ import org.eclipse.che.ide.util.loging.Log;
 
 import javax.validation.constraints.NotNull;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,16 +32,15 @@ import java.util.Map;
  */
 public class DevMachine {
 
-    private final MachineDto devMachineDescriptor;
-
+    private final MachineDto                    devMachineDescriptor;
     private final Map<String, DevMachineServer> servers;
-
-    private final Map<String, String> runtimeProperties;
-
-    private final Map<String, String> envVariables;
+    private final Map<String, String>           runtimeProperties;
+    private final Map<String, String>           envVariables;
+    private final List<Link>                    devMachineLinks;
 
     public DevMachine(@NotNull MachineDto devMachineDescriptor) {
         this.devMachineDescriptor = devMachineDescriptor;
+        this.devMachineLinks = devMachineDescriptor.getLinks();
 
         Map<String, ServerDto> serverDtoMap = devMachineDescriptor.getRuntime().getServers();
         servers = new HashMap<>(serverDtoMap.size());
@@ -49,13 +49,11 @@ public class DevMachine {
         }
         runtimeProperties = devMachineDescriptor.getRuntime().getProperties();
         envVariables = devMachineDescriptor.getRuntime().getEnvVariables();
-
     }
 
     public Map<String, String> getEnvVariables() {
         return envVariables;
     }
-
 
     public Map<String, String> getRuntimeProperties() {
         return runtimeProperties;
@@ -66,18 +64,27 @@ public class DevMachine {
     }
 
     public String getWsAgentWebSocketUrl() {
-        DevMachineServer server = getServer(Constants.WSAGENT_REFERENCE);
-        if (server != null) {
-            String url = server.getUrl();
-            String extUrl = url.substring(url.indexOf(':'), url.length());
-            final String protocol = Window.Location.getProtocol().equals("https:") ? "wss" : "ws";
-            return protocol + extUrl + (extUrl.endsWith("/") ? "ws" : "/ws");
-        } else {
-            //should not be
-            String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
-            Log.error(getClass(), message);
-            throw new RuntimeException(message);
+        for (Link link : devMachineLinks) {
+            if (Constants.WSAGENT_WEBSOCKET_REFERENCE.equals(link.getRel())) {
+                return link.getHref();
+            }
         }
+        //should not be
+        final String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
+        Log.error(getClass(), message);
+        throw new RuntimeException(message);
+    }
+
+    public String getTerminalUrl() {
+        for (Link link : devMachineLinks) {
+            if (Constants.TERMINAL_REFERENCE.equals(link.getRel())) {
+                return link.getHref();
+            }
+        }
+        //should not be
+        final String message = "Reference " + Constants.WSAGENT_REFERENCE + " not found in DevMachine description";
+        Log.error(getClass(), message);
+        throw new RuntimeException(message);
     }
 
     /**
@@ -104,9 +111,6 @@ public class DevMachine {
         return servers;
     }
 
-
-
-
     public DevMachineServer getServer(String reference) {
         if (!Strings.isNullOrEmpty(reference)) {
             for (DevMachineServer server : servers.values()) {
@@ -118,7 +122,7 @@ public class DevMachine {
         return null;
     }
 
-    public String getWorkspace(){
+    public String getWorkspace() {
         return devMachineDescriptor.getWorkspaceId();
     }
 

wsmaster/che-core-api-workspace/src/test/java/org/eclipse/che/api/workspace/server/WorkspaceServiceTest.java
@@ -51,6 +51,7 @@ import org.everrest.core.RequestFilter;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -108,11 +109,12 @@ import static org.testng.Assert.assertTrue;
 public class WorkspaceServiceTest {
 
     @SuppressWarnings("unused")
-    private static final ApiExceptionMapper MAPPER  = new ApiExceptionMapper();
-    private static final String             USER_ID = "user123";
-    private static final LinkedList<String> ROLES   = new LinkedList<>(singleton("user"));
+    private static final ApiExceptionMapper MAPPER      = new ApiExceptionMapper();
+    private static final String             USER_ID     = "user123";
+    private static final String             IDE_CONTEXT = "ws";
+    private static final LinkedList<String> ROLES       = new LinkedList<>(singleton("user"));
     @SuppressWarnings("unused")
-    private static final EnvironmentFilter  FILTER  = new EnvironmentFilter();
+    private static final EnvironmentFilter  FILTER      = new EnvironmentFilter();
 
     @Mock
     private WorkspaceManager   wsManager;
@@ -122,6 +124,11 @@ public class WorkspaceServiceTest {
     private WorkspaceValidator validator;
     @InjectMocks
     private WorkspaceService   service;
+
+    @BeforeMethod
+    public void setup() {
+        service = new WorkspaceService(wsManager, machineManager, validator, new WorkspaceServiceLinksInjector(IDE_CONTEXT));
+    }
 
     @Test
     public void shouldCreateWorkspace() throws Exception {