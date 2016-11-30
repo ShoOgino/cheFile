wzwflove/che

CHE-2937. Add ability to create batch of projects

ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/project/MutableProjectConfig.java
@@ -14,6 +14,7 @@ import com.google.common.annotations.Beta;
 
 import org.eclipse.che.api.core.model.project.ProjectConfig;
 import org.eclipse.che.api.core.model.project.SourceStorage;
+import org.eclipse.che.api.workspace.shared.dto.CreateProjectConfigDto;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,14 +30,15 @@ import static com.google.common.collect.Maps.newHashMap;
 @Beta
 public class MutableProjectConfig implements ProjectConfig {
 
-    private String                    name;
-    private String                    path;
-    private String                    description;
-    private String                    type;
-    private List<String>              mixins;
-    private Map<String, List<String>> attributes;
-    private MutableSourceStorage      sourceStorage;
-    private Map<String, String>       options;
+    private String                       name;
+    private String                       path;
+    private String                       description;
+    private String                       type;
+    private List<String>                 mixins;
+    private Map<String, List<String>>    attributes;
+    private MutableSourceStorage         sourceStorage;
+    private Map<String, String>          options;
+    private List<CreateProjectConfigDto> projects;
 
     public MutableProjectConfig(ProjectConfig source) {
         name = source.getName();
@@ -135,6 +137,28 @@ public class MutableProjectConfig implements ProjectConfig {
 
     public void setOptions(Map<String, String> options) {
         this.options = options;
+    }
+
+    /**
+     * Returns the list of configurations to creating projects
+     *
+     * @return the list of {@link CreateProjectConfigDto} to creating projects
+     */
+    public List<CreateProjectConfigDto> getProjects() {
+        if (projects == null) {
+            return new ArrayList<>();
+        }
+        return projects;
+    }
+
+    /**
+     * Sets the list of configurations to creating projects
+     *
+     * @param projects
+     *         the list of {@link CreateProjectConfigDto} to creating projects
+     */
+    public void setProjects(List<CreateProjectConfigDto> projects) {
+        this.projects = projects;
     }
 
     public class MutableSourceStorage implements SourceStorage {

ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/project/ProjectServiceClientImpl.java
@@ -26,6 +26,7 @@ import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.PromiseError;
 import org.eclipse.che.api.promises.client.callback.AsyncPromiseHelper;
 import org.eclipse.che.api.promises.client.callback.PromiseHelper;
+import org.eclipse.che.api.workspace.shared.dto.CreateProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.ProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.SourceStorageDto;
 import org.eclipse.che.ide.MimeType;
@@ -67,7 +68,8 @@ import static org.eclipse.che.ide.rest.HTTPHeader.CONTENT_TYPE;
  */
 public class ProjectServiceClientImpl implements ProjectServiceClient {
 
-    private static final String PROJECT = "/project";
+    private static final String PROJECT        = "/project";
+    private static final String BATCH_PROJECTS = "/batch";
 
     private static final String ITEM     = "/item";
     private static final String TREE     = "/tree";
@@ -213,6 +215,16 @@ public class ProjectServiceClientImpl implements ProjectServiceClient {
                          .header(ACCEPT, MimeType.APPLICATION_JSON)
                          .loader(loaderFactory.newLoader("Creating project..."))
                          .send(unmarshaller.newUnmarshaller(ProjectConfigDto.class));
+    }
+
+    @Override
+    public Promise<List<ProjectConfigDto>> createBatchProjects(List<CreateProjectConfigDto> configurations) {
+        final String url = getBaseUrl() + BATCH_PROJECTS;
+        final String loaderMessage = configurations.size() > 1 ? "Creating the batch of projects..." : "Creating project...";
+        return reqFactory.createPostRequest(url, configurations)
+                         .header(ACCEPT, MimeType.APPLICATION_JSON)
+                         .loader(loaderFactory.newLoader(loaderMessage))
+                         .send(unmarshaller.newListUnmarshaller(ProjectConfigDto.class));
     }
 
     /** {@inheritDoc} */

wsmaster/che-core-api-project-templates-shared/src/main/java/org/eclipse/che/api/project/templates/shared/dto/ProjectTemplateDescriptor.java
@@ -12,7 +12,7 @@ package org.eclipse.che.api.project.templates.shared.dto;
 
 import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.machine.shared.dto.CommandDto;
-import org.eclipse.che.api.workspace.shared.dto.ProjectConfigDto;
+import org.eclipse.che.api.workspace.shared.dto.CreateProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.ProjectProblemDto;
 import org.eclipse.che.api.workspace.shared.dto.SourceStorageDto;
 import org.eclipse.che.dto.shared.DTO;
@@ -110,4 +110,16 @@ public interface ProjectTemplateDescriptor {
     void setTags(List<String> tags);
 
     ProjectTemplateDescriptor withTags(List<String> tags);
+
+    List<CreateProjectConfigDto> getProjects();
+
+    void setProjects(List<CreateProjectConfigDto> projects);
+
+    ProjectTemplateDescriptor withProjects(List<CreateProjectConfigDto> projects);
+
+    Map<String, String> getOptions();
+
+    void setOptions(Map<String, String> options);
+
+    ProjectTemplateDescriptor withOptions(Map<String, String> options);
 }

ShoOgino/cheFile

ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/project/MutableProjectConfig.java
@@ -12,11 +12,10 @@ package org.eclipse.che.ide.api.project;
 
 import com.google.common.annotations.Beta;
 
+import org.eclipse.che.api.core.model.project.NewProjectConfig;
 import org.eclipse.che.api.core.model.project.ProjectConfig;
 import org.eclipse.che.api.core.model.project.SourceStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +36,7 @@ public class MutableProjectConfig implements ProjectConfig {
     private Map<String, List<String>> attributes;
     private MutableSourceStorage      sourceStorage;
     private Map<String, String>       options;
+    private List<NewProjectConfig>    projects;
 
     public MutableProjectConfig(ProjectConfig source) {
         name = source.getName();
@@ -90,7 +90,7 @@ public class MutableProjectConfig implements ProjectConfig {
     @Override
     public List<String> getMixins() {
         if (mixins == null) {
-            mixins = new ArrayList<>();
+            mixins = newArrayList();
         }
 
         return mixins;
@@ -128,13 +128,35 @@ public class MutableProjectConfig implements ProjectConfig {
 
     public Map<String, String> getOptions() {
         if (options == null) {
-            options = new HashMap<>();
+            options = newHashMap();
         }
         return options;
     }
 
     public void setOptions(Map<String, String> options) {
         this.options = options;
+    }
+
+    /**
+     * Returns the list of configurations to creating projects
+     *
+     * @return the list of {@link NewProjectConfig} to creating projects
+     */
+    public List<NewProjectConfig> getProjects() {
+        if (projects == null) {
+            return newArrayList();
+        }
+        return projects;
+    }
+
+    /**
+     * Sets the list of configurations to creating projects
+     *
+     * @param projects
+     *         the list of {@link NewProjectConfig} to creating projects
+     */
+    public void setProjects(List<NewProjectConfig> projects) {
+        this.projects = projects;
     }
 
     public class MutableSourceStorage implements SourceStorage {

ide/che-core-ide-api/src/main/java/org/eclipse/che/ide/api/project/ProjectServiceClientImpl.java
@@ -26,6 +26,7 @@ import org.eclipse.che.api.promises.client.Promise;
 import org.eclipse.che.api.promises.client.PromiseError;
 import org.eclipse.che.api.promises.client.callback.AsyncPromiseHelper;
 import org.eclipse.che.api.promises.client.callback.PromiseHelper;
+import org.eclipse.che.api.workspace.shared.dto.NewProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.ProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.SourceStorageDto;
 import org.eclipse.che.ide.MimeType;
@@ -65,7 +66,8 @@ import static org.eclipse.che.ide.rest.HTTPHeader.CONTENT_TYPE;
  */
 public class ProjectServiceClientImpl implements ProjectServiceClient {
 
-    private static final String PROJECT = "/project";
+    private static final String PROJECT        = "/project";
+    private static final String BATCH_PROJECTS = "/batch";
 
     private static final String ITEM     = "/item";
     private static final String TREE     = "/tree";
@@ -211,6 +213,16 @@ public class ProjectServiceClientImpl implements ProjectServiceClient {
                          .header(ACCEPT, MimeType.APPLICATION_JSON)
                          .loader(loaderFactory.newLoader("Creating project..."))
                          .send(unmarshaller.newUnmarshaller(ProjectConfigDto.class));
+    }
+
+    @Override
+    public Promise<List<ProjectConfigDto>> createBatchProjects(List<NewProjectConfigDto> configurations) {
+        final String url = getBaseUrl() + BATCH_PROJECTS;
+        final String loaderMessage = configurations.size() > 1 ? "Creating the batch of projects..." : "Creating project...";
+        return reqFactory.createPostRequest(url, configurations)
+                         .header(ACCEPT, MimeType.APPLICATION_JSON)
+                         .loader(loaderFactory.newLoader(loaderMessage))
+                         .send(unmarshaller.newListUnmarshaller(ProjectConfigDto.class));
     }
 
     /** {@inheritDoc} */

wsmaster/che-core-api-project-templates-shared/src/main/java/org/eclipse/che/api/project/templates/shared/dto/ProjectTemplateDescriptor.java
@@ -12,7 +12,7 @@ package org.eclipse.che.api.project.templates.shared.dto;
 
 import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.machine.shared.dto.CommandDto;
-import org.eclipse.che.api.workspace.shared.dto.ProjectConfigDto;
+import org.eclipse.che.api.workspace.shared.dto.NewProjectConfigDto;
 import org.eclipse.che.api.workspace.shared.dto.ProjectProblemDto;
 import org.eclipse.che.api.workspace.shared.dto.SourceStorageDto;
 import org.eclipse.che.dto.shared.DTO;
@@ -110,4 +110,16 @@ public interface ProjectTemplateDescriptor {
     void setTags(List<String> tags);
 
     ProjectTemplateDescriptor withTags(List<String> tags);
+
+    List<NewProjectConfigDto> getProjects();
+
+    void setProjects(List<NewProjectConfigDto> projects);
+
+    ProjectTemplateDescriptor withProjects(List<NewProjectConfigDto> projects);
+
+    Map<String, String> getOptions();
+
+    void setOptions(Map<String, String> options);
+
+    ProjectTemplateDescriptor withOptions(Map<String, String> options);
 }