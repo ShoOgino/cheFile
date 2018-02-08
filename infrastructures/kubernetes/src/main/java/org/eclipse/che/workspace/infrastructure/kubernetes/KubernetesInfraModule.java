sleshchenko/che

CHE-5908 Add Kubernetes infrastructure

infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/KubernetesInfraModule.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes;
+
+import static org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.CommonPVCStrategy.COMMON_STRATEGY;
+import static org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.UniqueWorkspacePVCStrategy.UNIQUE_STRATEGY;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.MapBinder;
+import com.google.inject.multibindings.Multibinder;
+import org.eclipse.che.api.workspace.server.spi.RuntimeInfrastructure;
+import org.eclipse.che.api.workspace.server.spi.environment.InternalEnvironmentFactory;
+import org.eclipse.che.api.workspace.server.spi.provision.env.CheApiEnvVarProvider;
+import org.eclipse.che.api.workspace.server.spi.provision.env.EnvVarProvider;
+import org.eclipse.che.workspace.infrastructure.docker.environment.dockerimage.DockerImageEnvironment;
+import org.eclipse.che.workspace.infrastructure.docker.environment.dockerimage.DockerImageEnvironmentFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.bootstrapper.KubernetesBootstrapperFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironment;
+import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironmentFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.RemoveNamespaceOnWorkspaceRemove;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.CommonPVCStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.UniqueWorkspacePVCStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.WorkspacePVCCleaner;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.WorkspaceVolumeStrategyProvider;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.pvc.WorkspaceVolumesStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.provision.KubernetesCheApiEnvVarProvider;
+import org.eclipse.che.workspace.infrastructure.kubernetes.provision.env.LogsRootEnvVariableProvider;
+
+/** @author Sergii Leshchenko */
+public class KubernetesInfraModule extends AbstractModule {
+  @Override
+  protected void configure() {
+    MapBinder<String, InternalEnvironmentFactory> factories =
+        MapBinder.newMapBinder(binder(), String.class, InternalEnvironmentFactory.class);
+
+    factories.addBinding(KubernetesEnvironment.TYPE).to(KubernetesEnvironmentFactory.class);
+    factories.addBinding(DockerImageEnvironment.TYPE).to(DockerImageEnvironmentFactory.class);
+
+    bind(RuntimeInfrastructure.class).to(KubernetesInfrastructure.class);
+
+    install(new FactoryModuleBuilder().build(KubernetesRuntimeContextFactory.class));
+
+    install(new FactoryModuleBuilder().build(KubernetesRuntimeFactory.class));
+    install(new FactoryModuleBuilder().build(KubernetesBootstrapperFactory.class));
+    bind(WorkspacePVCCleaner.class).asEagerSingleton();
+    bind(RemoveNamespaceOnWorkspaceRemove.class).asEagerSingleton();
+
+    bind(CheApiEnvVarProvider.class).to(KubernetesCheApiEnvVarProvider.class);
+
+    MapBinder<String, WorkspaceVolumesStrategy> volumesStrategies =
+        MapBinder.newMapBinder(binder(), String.class, WorkspaceVolumesStrategy.class);
+    volumesStrategies.addBinding(COMMON_STRATEGY).to(CommonPVCStrategy.class);
+    volumesStrategies.addBinding(UNIQUE_STRATEGY).to(UniqueWorkspacePVCStrategy.class);
+    bind(WorkspaceVolumesStrategy.class).toProvider(WorkspaceVolumeStrategyProvider.class);
+
+    Multibinder<EnvVarProvider> envVarProviders =
+        Multibinder.newSetBinder(binder(), EnvVarProvider.class);
+    envVarProviders.addBinding().to(LogsRootEnvVariableProvider.class);
+  }
+}

infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/project/KubernetesPersistentVolumeClaims.java
@@ -1,0 +1,110 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes.project;
+
+import static java.util.stream.Collectors.toSet;
+
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.workspace.infrastructure.kubernetes.KubernetesClientFactory;
+
+/**
+ * Defines an internal API for managing {@link PersistentVolumeClaim} instances in {@link
+ * KubernetesPersistentVolumeClaims#namespace predefined namespace}.
+ *
+ * @author Sergii Leshchenko
+ */
+public class KubernetesPersistentVolumeClaims {
+  private final String namespace;
+  private final KubernetesClientFactory clientFactory;
+
+  KubernetesPersistentVolumeClaims(String namespace, KubernetesClientFactory clientFactory) {
+    this.namespace = namespace;
+    this.clientFactory = clientFactory;
+  }
+
+  /**
+   * Creates specified persistent volume claim.
+   *
+   * @param pvc persistent volume claim to create
+   * @return created persistent volume claim
+   * @throws InfrastructureException when any exception occurs
+   */
+  public PersistentVolumeClaim create(PersistentVolumeClaim pvc) throws InfrastructureException {
+    try {
+      return clientFactory.create().persistentVolumeClaims().inNamespace(namespace).create(pvc);
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Returns all existing persistent volume claims.
+   *
+   * @throws InfrastructureException when any exception occurs
+   */
+  public List<PersistentVolumeClaim> get() throws InfrastructureException {
+    try {
+      return clientFactory
+          .create()
+          .persistentVolumeClaims()
+          .inNamespace(namespace)
+          .list()
+          .getItems();
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Returns all existing persistent volume claims with given label.
+   *
+   * @param labelName name of provided label
+   * @param labelValue value of provided label
+   * @return list of matched PVCs
+   * @throws InfrastructureException when any exception occurs while fetching the pvcs
+   */
+  public List<PersistentVolumeClaim> getByLabel(String labelName, String labelValue)
+      throws InfrastructureException {
+    try {
+      return clientFactory
+          .create()
+          .persistentVolumeClaims()
+          .inNamespace(namespace)
+          .withLabel(labelName, labelValue)
+          .list()
+          .getItems();
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Creates all PVCs which are not present in current Kubernetes project.
+   *
+   * @param toCreate collection of PVCs to create
+   * @throws InfrastructureException when any error occurs while creation
+   */
+  public void createIfNotExist(Collection<PersistentVolumeClaim> toCreate)
+      throws InfrastructureException {
+    final Set<String> existing =
+        get().stream().map(p -> p.getMetadata().getName()).collect(toSet());
+    for (PersistentVolumeClaim pvc : toCreate) {
+      if (!existing.contains(pvc.getMetadata().getName())) {
+        create(pvc);
+      }
+    }
+  }
+}

infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/KubernetesCheApiEnvVarProviderTest.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes.provision;
+
+import static org.testng.Assert.assertEquals;
+
+import org.eclipse.che.commons.lang.Pair;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+/**
+ * Tests {@link KubernetesCheApiEnvVarProvider}.
+ *
+ * @author Sergii Leshchenko
+ */
+@Listeners(MockitoTestNGListener.class)
+public class KubernetesCheApiEnvVarProviderTest {
+
+  private static final String CHE_SERVER_ENDPOINT = "localhost:8080";
+
+  private KubernetesCheApiEnvVarProvider openShiftCheApiEnvVarProvider;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    openShiftCheApiEnvVarProvider = new KubernetesCheApiEnvVarProvider(CHE_SERVER_ENDPOINT);
+  }
+
+  @Test
+  public void shouldReturnCheApiEnv() throws Exception {
+    // when
+    Pair<String, String> cheApiEnv = openShiftCheApiEnvVarProvider.get(null);
+
+    // then
+    assertEquals(cheApiEnv.first, KubernetesCheApiEnvVarProvider.API_ENDPOINT_URL_VARIABLE);
+    assertEquals(cheApiEnv.second, CHE_SERVER_ENDPOINT);
+  }
+}

infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/project/OpenShiftProjectTest.java
@@ -28,6 +28,10 @@ import io.fabric8.openshift.api.model.ProjectRequestFluent.MetadataNested;
 import io.fabric8.openshift.client.OpenShiftClient;
 import io.fabric8.openshift.client.dsl.ProjectRequestOperation;
 import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.KubernetesIngresses;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.KubernetesPersistentVolumeClaims;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.KubernetesPods;
+import org.eclipse.che.workspace.infrastructure.kubernetes.project.KubernetesServices;
 import org.eclipse.che.workspace.infrastructure.openshift.OpenShiftClientFactory;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
@@ -46,22 +50,22 @@ public class OpenShiftProjectTest {
   public static final String PROJECT_NAME = "testProject";
   public static final String WORKSPACE_ID = "workspace123";
 
-  @Mock private OpenShiftPods pods;
-  @Mock private OpenShiftServices services;
+  @Mock private KubernetesPods pods;
+  @Mock private KubernetesServices services;
   @Mock private OpenShiftRoutes routes;
-  @Mock private OpenShiftPersistentVolumeClaims pvcs;
+  @Mock private KubernetesPersistentVolumeClaims pvcs;
+  @Mock private KubernetesIngresses ingresses;
   @Mock private OpenShiftClientFactory clientFactory;
   @Mock private OpenShiftClient openShiftClient;
-  @Mock private KubernetesIngress kubernetesIngress;
 
   private OpenShiftProject openShiftProject;
 
   @BeforeMethod
   public void setUp() throws Exception {
     when(clientFactory.create()).thenReturn(openShiftClient);
+    when(openShiftClient.adapt(OpenShiftClient.class)).thenReturn(openShiftClient);
 
-    openShiftProject =
-        new OpenShiftProject(WORKSPACE_ID, pods, services, routes, pvcs, kubernetesIngress);
+    openShiftProject = new OpenShiftProject(WORKSPACE_ID, pods, services, routes, pvcs, ingresses);
   }
 
   @Test
@@ -94,15 +98,15 @@ public class OpenShiftProjectTest {
     // when
     openShiftProject.cleanUp();
 
-    verify(pods).delete();
-    verify(services).delete();
     verify(routes).delete();
+    verify(services).delete();
+    verify(pods).delete();
   }
 
   @Test
   public void testOpenShiftProjectCleaningUpIfExceptionsOccurs() throws Exception {
-    doThrow(new InfrastructureException("err1.")).when(pods).delete();
-    doThrow(new InfrastructureException("err2.")).when(services).delete();
+    doThrow(new InfrastructureException("err1.")).when(services).delete();
+    doThrow(new InfrastructureException("err2.")).when(pods).delete();
 
     InfrastructureException error = null;
     // when

ShoOgino/cheFile

infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/KubernetesInfraModule.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes;
+
+import static org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.CommonPVCStrategy.COMMON_STRATEGY;
+import static org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.UniqueWorkspacePVCStrategy.UNIQUE_STRATEGY;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.MapBinder;
+import com.google.inject.multibindings.Multibinder;
+import org.eclipse.che.api.workspace.server.spi.RuntimeInfrastructure;
+import org.eclipse.che.api.workspace.server.spi.environment.InternalEnvironmentFactory;
+import org.eclipse.che.api.workspace.server.spi.provision.env.CheApiEnvVarProvider;
+import org.eclipse.che.api.workspace.server.spi.provision.env.EnvVarProvider;
+import org.eclipse.che.workspace.infrastructure.docker.environment.dockerimage.DockerImageEnvironment;
+import org.eclipse.che.workspace.infrastructure.docker.environment.dockerimage.DockerImageEnvironmentFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.bootstrapper.KubernetesBootstrapperFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironment;
+import org.eclipse.che.workspace.infrastructure.kubernetes.environment.KubernetesEnvironmentFactory;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.RemoveNamespaceOnWorkspaceRemove;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.CommonPVCStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.UniqueWorkspacePVCStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.WorkspacePVCCleaner;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.WorkspaceVolumeStrategyProvider;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.pvc.WorkspaceVolumesStrategy;
+import org.eclipse.che.workspace.infrastructure.kubernetes.provision.KubernetesCheApiEnvVarProvider;
+import org.eclipse.che.workspace.infrastructure.kubernetes.provision.env.LogsRootEnvVariableProvider;
+
+/** @author Sergii Leshchenko */
+public class KubernetesInfraModule extends AbstractModule {
+  @Override
+  protected void configure() {
+    MapBinder<String, InternalEnvironmentFactory> factories =
+        MapBinder.newMapBinder(binder(), String.class, InternalEnvironmentFactory.class);
+
+    factories.addBinding(KubernetesEnvironment.TYPE).to(KubernetesEnvironmentFactory.class);
+    factories.addBinding(DockerImageEnvironment.TYPE).to(DockerImageEnvironmentFactory.class);
+
+    bind(RuntimeInfrastructure.class).to(KubernetesInfrastructure.class);
+
+    install(new FactoryModuleBuilder().build(KubernetesRuntimeContextFactory.class));
+
+    install(new FactoryModuleBuilder().build(KubernetesRuntimeFactory.class));
+    install(new FactoryModuleBuilder().build(KubernetesBootstrapperFactory.class));
+    bind(WorkspacePVCCleaner.class).asEagerSingleton();
+    bind(RemoveNamespaceOnWorkspaceRemove.class).asEagerSingleton();
+
+    bind(CheApiEnvVarProvider.class).to(KubernetesCheApiEnvVarProvider.class);
+
+    MapBinder<String, WorkspaceVolumesStrategy> volumesStrategies =
+        MapBinder.newMapBinder(binder(), String.class, WorkspaceVolumesStrategy.class);
+    volumesStrategies.addBinding(COMMON_STRATEGY).to(CommonPVCStrategy.class);
+    volumesStrategies.addBinding(UNIQUE_STRATEGY).to(UniqueWorkspacePVCStrategy.class);
+    bind(WorkspaceVolumesStrategy.class).toProvider(WorkspaceVolumeStrategyProvider.class);
+
+    Multibinder<EnvVarProvider> envVarProviders =
+        Multibinder.newSetBinder(binder(), EnvVarProvider.class);
+    envVarProviders.addBinding().to(LogsRootEnvVariableProvider.class);
+  }
+}

infrastructures/kubernetes/src/main/java/org/eclipse/che/workspace/infrastructure/kubernetes/namespace/KubernetesPersistentVolumeClaims.java
@@ -1,0 +1,110 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes.namespace;
+
+import static java.util.stream.Collectors.toSet;
+
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.workspace.infrastructure.kubernetes.KubernetesClientFactory;
+
+/**
+ * Defines an internal API for managing {@link PersistentVolumeClaim} instances in {@link
+ * KubernetesPersistentVolumeClaims#namespace predefined namespace}.
+ *
+ * @author Sergii Leshchenko
+ */
+public class KubernetesPersistentVolumeClaims {
+  private final String namespace;
+  private final KubernetesClientFactory clientFactory;
+
+  KubernetesPersistentVolumeClaims(String namespace, KubernetesClientFactory clientFactory) {
+    this.namespace = namespace;
+    this.clientFactory = clientFactory;
+  }
+
+  /**
+   * Creates specified persistent volume claim.
+   *
+   * @param pvc persistent volume claim to create
+   * @return created persistent volume claim
+   * @throws InfrastructureException when any exception occurs
+   */
+  public PersistentVolumeClaim create(PersistentVolumeClaim pvc) throws InfrastructureException {
+    try {
+      return clientFactory.create().persistentVolumeClaims().inNamespace(namespace).create(pvc);
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Returns all existing persistent volume claims.
+   *
+   * @throws InfrastructureException when any exception occurs
+   */
+  public List<PersistentVolumeClaim> get() throws InfrastructureException {
+    try {
+      return clientFactory
+          .create()
+          .persistentVolumeClaims()
+          .inNamespace(namespace)
+          .list()
+          .getItems();
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Returns all existing persistent volume claims with given label.
+   *
+   * @param labelName name of provided label
+   * @param labelValue value of provided label
+   * @return list of matched PVCs
+   * @throws InfrastructureException when any exception occurs while fetching the pvcs
+   */
+  public List<PersistentVolumeClaim> getByLabel(String labelName, String labelValue)
+      throws InfrastructureException {
+    try {
+      return clientFactory
+          .create()
+          .persistentVolumeClaims()
+          .inNamespace(namespace)
+          .withLabel(labelName, labelValue)
+          .list()
+          .getItems();
+    } catch (KubernetesClientException e) {
+      throw new InfrastructureException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Creates all PVCs which are not present in current Kubernetes namespace.
+   *
+   * @param toCreate collection of PVCs to create
+   * @throws InfrastructureException when any error occurs while creation
+   */
+  public void createIfNotExist(Collection<PersistentVolumeClaim> toCreate)
+      throws InfrastructureException {
+    final Set<String> existing =
+        get().stream().map(p -> p.getMetadata().getName()).collect(toSet());
+    for (PersistentVolumeClaim pvc : toCreate) {
+      if (!existing.contains(pvc.getMetadata().getName())) {
+        create(pvc);
+      }
+    }
+  }
+}

infrastructures/kubernetes/src/test/java/org/eclipse/che/workspace/infrastructure/kubernetes/provision/KubernetesCheApiEnvVarProviderTest.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package org.eclipse.che.workspace.infrastructure.kubernetes.provision;
+
+import static org.testng.Assert.assertEquals;
+
+import org.eclipse.che.commons.lang.Pair;
+import org.mockito.testng.MockitoTestNGListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+/**
+ * Tests {@link KubernetesCheApiEnvVarProvider}.
+ *
+ * @author Sergii Leshchenko
+ */
+@Listeners(MockitoTestNGListener.class)
+public class KubernetesCheApiEnvVarProviderTest {
+
+  private static final String CHE_SERVER_ENDPOINT = "localhost:8080";
+
+  private KubernetesCheApiEnvVarProvider kubernetesCheApiEnvVarProvider;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    kubernetesCheApiEnvVarProvider = new KubernetesCheApiEnvVarProvider(CHE_SERVER_ENDPOINT);
+  }
+
+  @Test
+  public void shouldReturnCheApiEnv() throws Exception {
+    // when
+    Pair<String, String> cheApiEnv = kubernetesCheApiEnvVarProvider.get(null);
+
+    // then
+    assertEquals(cheApiEnv.first, KubernetesCheApiEnvVarProvider.API_ENDPOINT_URL_VARIABLE);
+    assertEquals(cheApiEnv.second, CHE_SERVER_ENDPOINT);
+  }
+}

infrastructures/openshift/src/test/java/org/eclipse/che/workspace/infrastructure/openshift/project/OpenShiftProjectTest.java
@@ -28,6 +28,10 @@ import io.fabric8.openshift.api.model.ProjectRequestFluent.MetadataNested;
 import io.fabric8.openshift.client.OpenShiftClient;
 import io.fabric8.openshift.client.dsl.ProjectRequestOperation;
 import org.eclipse.che.api.workspace.server.spi.InfrastructureException;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.KubernetesIngresses;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.KubernetesPersistentVolumeClaims;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.KubernetesPods;
+import org.eclipse.che.workspace.infrastructure.kubernetes.namespace.KubernetesServices;
 import org.eclipse.che.workspace.infrastructure.openshift.OpenShiftClientFactory;
 import org.mockito.Mock;
 import org.mockito.testng.MockitoTestNGListener;
@@ -46,22 +50,22 @@ public class OpenShiftProjectTest {
   public static final String PROJECT_NAME = "testProject";
   public static final String WORKSPACE_ID = "workspace123";
 
-  @Mock private OpenShiftPods pods;
-  @Mock private OpenShiftServices services;
+  @Mock private KubernetesPods pods;
+  @Mock private KubernetesServices services;
   @Mock private OpenShiftRoutes routes;
-  @Mock private OpenShiftPersistentVolumeClaims pvcs;
+  @Mock private KubernetesPersistentVolumeClaims pvcs;
+  @Mock private KubernetesIngresses ingresses;
   @Mock private OpenShiftClientFactory clientFactory;
   @Mock private OpenShiftClient openShiftClient;
-  @Mock private KubernetesIngress kubernetesIngress;
 
   private OpenShiftProject openShiftProject;
 
   @BeforeMethod
   public void setUp() throws Exception {
     when(clientFactory.create()).thenReturn(openShiftClient);
+    when(openShiftClient.adapt(OpenShiftClient.class)).thenReturn(openShiftClient);
 
-    openShiftProject =
-        new OpenShiftProject(WORKSPACE_ID, pods, services, routes, pvcs, kubernetesIngress);
+    openShiftProject = new OpenShiftProject(WORKSPACE_ID, pods, services, routes, pvcs, ingresses);
   }
 
   @Test
@@ -94,15 +98,15 @@ public class OpenShiftProjectTest {
     // when
     openShiftProject.cleanUp();
 
-    verify(pods).delete();
-    verify(services).delete();
     verify(routes).delete();
+    verify(services).delete();
+    verify(pods).delete();
   }
 
   @Test
   public void testOpenShiftProjectCleaningUpIfExceptionsOccurs() throws Exception {
-    doThrow(new InfrastructureException("err1.")).when(pods).delete();
-    doThrow(new InfrastructureException("err2.")).when(services).delete();
+    doThrow(new InfrastructureException("err1.")).when(services).delete();
+    doThrow(new InfrastructureException("err2.")).when(pods).delete();
 
     InfrastructureException error = null;
     // when
@@ -116,7 +120,7 @@ public class OpenShiftProjectTest {
     // then
     assertNotNull(error);
     String message = error.getMessage();
-    assertEquals(message, "Error(s) occurs while cleaning project up. err1. err2.");
+    assertEquals(message, "Error(s) occurs while cleaning up the namespace. err1. err2.");
     verify(routes).delete();
   }
 